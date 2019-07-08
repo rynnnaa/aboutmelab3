cccc64aba09d3b63a78d9d98b64067da5ef50daa
Ryna <pinktulips@Hotmail.com>
2019-07-08 16:07:41 -0700

    adding conditionals

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 'use strict';
 
-alert('Hello. Welcome to my Yes/No Game. Please answer yes or no.');
-var answerOne = prompt('Is global warming real?').toLowerCase();
-console.log('global warming', answerOne);
-alert('Global warming is real and I respect your opinion.');
-var answerTwo = prompt('Is it possible to lick your elbow?').toLowerCase();
-console.log('elbow', answerTwo);
-alert('It is possuble, and I respect your opinion');
-var answerThree = prompt('Is Christmas the best holiday?').toLowerCase();
-console.log('christmas', answerThree);
+alert('Hello. Answer some questions to learn a little bit about me');
+var answerOne = prompt('Do I like spam? Please answer yes or no.').toLowerCase();
+var responseOne = 'Yes'.toLowerCase();
+if (answerOne === responseOne){
+  alert('yes, you are correct, one point for you!');
+}
+else {
+  alert('Who doesn\'t like spam? You get no points!');
+}
+console.log('spam', answerOne);
+console.log('yes', responseOne);
+
+var answerTwo = prompt('How many toes do I have?').toLowerCase();
+var responseTwo = '10'.parseInt();
+if (answerTwo === responseTwo){
+  alert('That\'s right, I have 10 toes.');
+}
+else {
+  alert('No, I have 10 toes.');
+}
+console.log('two', answerTwo);
+console.log('10', responseTwo);
+
+
+alert('I have 10');
+var answerThree = prompt('What is my favorite food?').toLowerCase();
+console.log('vietnameseFood', answerThree);
 alert('Christmas is alright, Thanksgiving is where it\'s at');
-var answerFour = prompt('Is water consumption necessary?').toLowerCase();
-console.log('water', answerFour);
+var answerFour = prompt('What is my favorite show right now?').toLowerCase();
+console.log('90DayFiance', answerFour);
 alert('Water consumption is definitely necessary. Otherwise, you die.');
-var answerFive = prompt('Do people have gills?').toLowerCase();
+var answerFive = prompt('Am I good at sports?').toLowerCase();
 console.log('gills', answerFive);
 alert('No, but that\'s your opinion');
