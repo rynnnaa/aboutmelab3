c922695be549c7da554c57d311da27c6790b4da0
Ryna <pinktulips@Hotmail.com>
2019-07-08 15:45:53 -0700

    add my top 10

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+alert('Hello. Welcome to my Yes/No Game. Please answer yes or no.');
+var answerOne = prompt('Is global warming real?').toLowerCase();
+console.log('global warming', answerOne);
+alert('Global warming is real and I respect your opinion.');
+var answerTwo = prompt('Is it possible to lick your elbow?').toLowerCase();
+console.log('elbow', answerTwo);
+alert('It is possuble, and I respect your opinion');
+var answerThree = prompt('Is Christmas the best holiday?').toLowerCase();
+console.log('christmas', answerThree);
+alert('Christmas is alright, Thanksgiving is where it\'s at');
+var answerFour = prompt('Is water consumption necessary?').toLowerCase();
+console.log('water', answerFour);
+alert('Water consumption is definitely necessary. Otherwise, you die.');
+var answerFive = prompt('Do people have gills?').toLowerCase();
+console.log('gills', answerFive);
+alert('No, but that\'s your opinion');
